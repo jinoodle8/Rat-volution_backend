df1e09ff03026ac9581e5ca7e22dbd1abdd20953
jinoodle8 <jh0308036@naver.com>
2026-05-09 20:46:24 +0900

    도감 서버 api 추가

--- routes/card.js
+++ routes/card.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const CardMaster = require('../models/CardMaster')
+const User = require('../models/User')
 
 // 마스터 데이터 전체 조회 GET /card/master
 // 게임 초기 접속 시 1회 전송 (REQ-046)
@@ -33,6 +34,51 @@ router.get('/master/:code', async (req, res) => {
         res.status(200).json({
             message: '카드 조회 성공',
             card
+        })
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message: '서버 오류' })
+    }
+})
+
+// 도감 조회 GET /card/dex/:userId  (REQ-038, REQ-039)
+// 유저의 획득 여부와 마스터 카드 데이터를 합쳐서 반환
+router.get('/dex/:userId', async (req, res) => {
+    try {
+        const { userId } = req.params
+
+        // 유저 + 마스터 카드 병렬 조회
+        const [user, allCards] = await Promise.all([
+            User.findById(userId).select('nickname discovered_cards'),
+            CardMaster.find().select('-__v').sort({ type: 1, rarity: 1, code: 1 })
+        ])
+
+        if (!user) {
+            return res.status(404).json({ message: '유저를 찾을 수 없습니다' })
+        }
+
+        // 획득 여부 매핑
+        const discoveredSet = new Set(user.discovered_cards)
+        const cards = allCards.map(card => ({
+            code: card.code,
+            name: card.name,
+            type: card.type,
+            item_type: card.item_type,
+            rarity: card.rarity,
+            description: card.description,
+            base_value: card.base_value,
+            scale_per_stack: card.scale_per_stack,
+            max_stack: card.max_stack,
+            discovered: discoveredSet.has(card.code)
+        }))
+
+        res.status(200).json({
+            message: '도감 조회 성공',
+            nickname: user.nickname,
+            total_cards: allCards.length,
+            discovered_count: user.discovered_cards.length,
+            cards
         })
 
     } catch (err) {
